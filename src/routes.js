8e71a0f08cfa1417fd74a17e9c514acc188503f3
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-05-11 11:21:51 -0300

    redirecionamento e title

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import Register from './pages/Register';
 
@@ -7,7 +7,8 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/register" component= { Register }/>
+        <Route path="/register" component={Register} />
+        <Redirect from='*' to='/register' />
       </Switch>
     </BrowserRouter>
   )
